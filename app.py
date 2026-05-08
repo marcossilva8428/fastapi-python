9671eb43e6e932ed541e753e4249b09d9b1a2351
Marcos Silva <marcos.da.silva.j@accenture.com>
2026-05-08 15:11:06 -0300

    Adicionando schema de mensagem

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+
+from schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/', response_class=HTMLResponse)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
+    return {'message': 'Olá Mundo!'}
+
+
+@app.get('/olamundohtml', response_class=HTMLResponse)
+def read_olamundohtml():
     return """
-<html>
-    <head>
-        <title>Nosso olá mundo!</title>
-    </head>
-    <body>
-        <h1>Olá Mundo</h1>
-    </body>
-</html>"""
+    <html>
+        <head>
+            <title>Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1>Olá Mundo</h1>
+        </body>
+    </html>
+"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
 from http import HTTPStatus
+
 from fastapi.testclient import TestClient
-import app
+
+from app import app
+
+client = TestClient(app)
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
@@ -10,4 +14,23 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
-    
+
+
+def test_olamundohtml_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/olamundohtml')
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+        <head>
+            <title>Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1>Olá Mundo</h1>
+        </body>
+    </html>
+"""
+    )
